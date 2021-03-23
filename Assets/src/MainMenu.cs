b08deb2c69330519e76b066a7e6356c6840df9d4
mench <mench@RokuStreamingStickPlus4K.myfiosgateway.com>
2021-03-23 15:17:41 -0400

    Committer: Ryu  Run main menu and play background music loop. Once player hits start, load main scene. Player can walk around and look around.  On branch master  Changes to be committed: 	new file:   Assets/Scenes/MainScene.unity 	new file:   Assets/Scenes/MainScene.unity.meta 	new file:   Assets/Scenes/Menu.unity 	new file:   Assets/Scenes/Menu.unity.meta 	new file:   Assets/Tree.prefab 	new file:   Assets/Tree.prefab.meta 	new file:   Assets/Tree_Textures.meta 	new file:   Assets/Tree_Textures/diffuse.png 	new file:   Assets/Tree_Textures/diffuse.png.meta 	new file:   Assets/Tree_Textures/normal_specular.png 	new file:   Assets/Tree_Textures/normal_specular.png.meta 	new file:   Assets/Tree_Textures/shadow.png 	new file:   Assets/Tree_Textures/shadow.png.meta 	new file:   Assets/Tree_Textures/translucency_gloss.png 	new file:   Assets/Tree_Textures/translucency_gloss.png.meta 	new file:   Assets/resources/audio/Nostalgia.wav 	new file:   Assets/resources/audio/Nostalgia.wav.meta 	new file:   Assets/src/MainMenu.cs 	new file:   Assets/src/MainMenu.cs.meta 	new file:   Assets/src/NC_CharacterController.cs 	new file:   Assets/src/NC_CharacterController.cs.meta

---
+++ Assets/src/MainMenu.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+[RequireComponent(typeof(AudioSource))]
+public class MainMenu : MonoBehaviour
+{
+
+
+    public AudioClip menuBackgroundMusic;
+    public AudioClip breathingSound;
+    public Button startButton;
+    public float startingPitch;
+    public float decreasePitchBy;
+    private AudioSource audioSource;
+    private bool detune = false;
+    private bool isStartingGame = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(detuneMusic);
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.pitch = startingPitch;
+        playMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //While detune, decreasePitchBy
+        if (detune)
+        {
+            if (audioSource.pitch > 0)
+            {
+                audioSource.pitch -= Time.deltaTime * decreasePitchBy;
+            }
+            else
+            {
+                audioSource.Stop();
+                audioSource.pitch = startingPitch;
+                StartCoroutine(playBreathSound());
+                detune = false;
+            }
+        }
+
+
+        if (isStartingGame)
+            startGame();
+    }
+
+
+    ///<summary>Load the main scene.</summary>
+    private void startGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+
+    ///<summary>Play the main menu background music.</summary>
+    private void playMusic()
+    {
+        audioSource.clip = menuBackgroundMusic;
+        audioSource.Play();
+    }
+
+
+    ///<summary>Play the breathing sound. Once it's finished, set isStartingGame true.</summary>
+    private IEnumerator playBreathSound()
+    {
+        audioSource.loop = false;
+        audioSource.clip = breathingSound;
+        audioSource.Play();
+
+
+        //Wait til audioSource finishes playing.
+        yield return new WaitWhile(()=> audioSource.isPlaying);
+        //start the next scene
+        isStartingGame = true;
+    }
+
+
+    ///<summary>Set detune to true. Detunes music during update calls.</summary>
+    private void detuneMusic()
+    {
+        detune = true;
+    }
+}
---
+++ Assets/src/NC_CharacterController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class NC_CharacterController : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+    public Camera playerCamera;
+    public float lookSpeed;
+    public float lookXLimit;
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector2 rotation = Vector2.zero;
+    private bool canMove = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        rotation.y = transform.eulerAngles.y;
+
+        //Keep the mouse inside the game screen
+        Cursor.lockState = CursorLockMode.Locked;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            //Recalculate direction
+            try
+            {
+                Vector3 forward = transform.TransformDirection(Vector3.forward);
+                Vector3 right = transform.TransformDirection(Vector3.right);
+                float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
+                float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
+                moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+
+
+            if (Input.GetButton("Jump") && canMove)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+
+        //Apply gravity. Gravity is multiplied by deltaTime twice.
+        moveDirection.y -= gravity * Time.deltaTime;
+
+
+        //Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+
+        //Player and Camera rotation
+        rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+        rotation.x -= Input.GetAxis("Mouse Y") * lookSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+        transform.eulerAngles = new Vector2(0, rotation.y);
+
+
+        //TODO other player keys should be added below. 
+    }
+}
