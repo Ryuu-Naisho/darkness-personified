93ac10bf4d2361e71072464873def245024cc522
mench <mench@RokuStreamingStickPlus4K.myfiosgateway.com>
2021-03-25 18:22:52 -0400

    Committer: Ryuu   Create ShapeShifter trigger and class. Modify Game events.   On branch master   Changes to be committed: 	modified:   Assets/src/GameEvents.cs 	new file:   Assets/src/ShapeShifter.cs 	new file:   Assets/src/ShapeShifter.cs.meta 	new file:   Assets/src/ShapeShifterTrigger.cs 	new file:   Assets/src/ShapeShifterTrigger.cs.meta

---
+++ Assets/src/ShapeShifter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeShifter : MonoBehaviour
+{
+
+
+    public Mesh shiftTo;
+    private MeshFilter meshFilter;
+    private int parentID;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        parentID = transform.parent.gameObject.GetInstanceID();
+        GameEvents.events.OnShapeShiftTrigger += OnShapeShift;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    ///<summary>Begin shape shift event.</summary>
+    private void OnShapeShift(int id)
+    {
+        if (id == parentID)
+            Shift();
+    }
+
+
+    private void Shift()
+    {
+        Mesh mesh = Instantiate(shiftTo);
+        meshFilter.sharedMesh = mesh;
+    }
+}
---
+++ Assets/src/ShapeShifterTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeShifterTrigger : MonoBehaviour
+{
+
+
+    private int parentID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentID = transform.parent.gameObject.GetInstanceID();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    ///<summary>Trigger when a player is in proximity.</summary>
+    private void OnTriggerEnter(Collider other)
+    {
+        GameEvents.events.ShapeShiftTrigger(parentID);
+    }
+}
