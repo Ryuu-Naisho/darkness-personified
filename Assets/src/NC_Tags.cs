bb15a05f033673f66a67f109596385b3b060c91e
mench <mench@RokuStreamingStickPlus4K.myfiosgateway.com>
2021-03-27 20:23:01 -0400

    Committer: Ryuu   Create tagging system.   On branch master   Changes to be committed: 	new file:   Assets/src/NC_Tags.cs 	new file:   Assets/src/NC_Tags.cs.meta

---
+++ Assets/src/NC_Tags.cs
@@ -0,0 +1,34 @@
+﻿
+public class NC_Tags
+{
+    private string _Door = "Door";
+    private string _PickUpItem = "PickUpItem";
+    private string _Player = "Player";
+    private string _ShapeShifter = "ShapeShifter";
+
+
+
+
+    public string Door
+    {
+        get => this._Door;
+    }
+
+
+    public string Player
+    {
+        get => this._Player;
+    }
+
+
+    public string PickUpItem
+    {
+        get => this.PickUpItem;
+    }
+
+
+    public string ShapeShifter
+    {
+        get => this._ShapeShifter;
+    }
+}
